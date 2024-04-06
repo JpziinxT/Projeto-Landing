73b3024c427c605aeba8d824ffa22681c5996a6f
j.pauloxiv <joaopauloaulasbv@gmail.com>
2024-04-06 16:25:07 -0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Roboto': 'Roboto',
+      },
+      colors: {
+        'primary': '#5E3BEE',
+        'secondary': '#F5FCFF',
+        'font': '#282938',
+        'font2': '#1C1E53',
+        'drible': '#E62872',
+        'border': '#006b6a',
+      },
+    },
+  },
+  plugins: [],
+}
+
